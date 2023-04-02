e556b580eda13cade6ac00a6282f8dac41b73511
Pyae Phyo Kyaw <pyaephyokyaw@better.hr>
2023-04-02 11:18:08 +0630

    Initial Commit

--- app/src/main/java/com/example/myapp/profile/HomeActivity.java
+++ app/src/main/java/com/example/myapp/profile/HomeActivity.java
@@ -7,24 +7,70 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.WebView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.myapp.R;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class HomeActivity extends AppCompatActivity {
 
     private static final String TAG = "HomeActivity";
+    private ListView listView;
+    private ArrayList<String> foods ;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Log.e(TAG,"this is onCreate");
+        Log.i(TAG,"this is onCreate");
+
 
         Bundle b = getIntent().getExtras();
-       // String userName = b.getString("username");
+        // String userName = b.getString("username");
+
+        foods = new ArrayList<>();
+        foods.add("Burger");
+        foods.add("Fries");
+        foods.add("Cheese");
+        foods.add("Burger");
+        foods.add("Fries");
+        foods.add("Cheese");
+        foods.add("Burger");
+        foods.add("Fries");
+        foods.add("Cheese");
+        foods.add("Burger");
+        foods.add("Fries");
+        foods.add("Cheese");
+        foods.add("Burger");
+        foods.add("Fries");
+        foods.add("Cheese");
+        foods.add("Milk");
+        foods.add("Egg");
+        foods.add("Bread");
+        foods.add("Cola");
+        foods.add("Fish");
+        foods.add("Beef");
+        foods.add("Salad");
+        foods.add("Rice");
+        foods.add("Ramen");
+        initViews();
     }
+
+    private void initViews(){
+        listView = findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,foods);
+        arrayAdapter.notifyDataSetChanged();
+
+        listView.setAdapter(arrayAdapter);
+    }
+
+
 
     @Override
     protected void onStart() {
